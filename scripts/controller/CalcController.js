7847b66036e804be22a2359ae7eef122eaf85f77
williangomes7004 <williangomes7004@gmail.com>
2019-05-21 00:46:44 -0300

    fucionalidade do botao =

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -84,17 +86,39 @@ class CalcController {
 
     }
 
+    getResult () {
+
+        return eval(this._operation.join(""));
+
+    }
+
     calc(){
 
         let last = '';
+
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3){
+
+            let firstItem = this._operation[0];
+
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
+
 
         if (this._operation.length > 3) {
 
             last = this._operation.pop();
 
+            this._lastNumber = this.getResult()
+
+        } else if (this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false);
+
         }
 
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if (last == '%') {
 
@@ -114,21 +138,34 @@ class CalcController {
 
     }
 
-    setLastNumberToDisplay() {
+    getLastItem(isOperator = true) {
 
-        let lastNumber;
+        let lastItem;
 
-        for(let i = this._operation.length-1; i >= 0; i--) {
-            
-            if (!this.isOperator(this._operation[i])) {
+        for (let i = this._operation.length - 1; i >= 0; i--) {
 
-                lastNumber = this._operation[i];
+            if (this.isOperator(this._operation[i]) == isOperator) {
+
+                lastItem = this._operation[i];
 
                 break;
 
             }
 
         }
+
+        if(!lastItem) {
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber = this.getLastItem(false);
 
         if(!lastNumber) lastNumber = 0;
 
